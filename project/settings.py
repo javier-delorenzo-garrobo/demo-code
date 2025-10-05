9748dd02fc8d6abad7734d1891b98a8897a42087
javier-delorenzo-garrobo <150132750+javier-delorenzo-garrobo@users.noreply.github.com>
2025-10-05 17:58:50 +0200

    Change database engine to SQLite
    Switched database backend from PostgreSQL to SQLite.

--- project/settings.py
+++ project/settings.py
@@ -84,14 +84,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME'),
-        'HOST': os.environ.get('DBHOST'),
-        'USER': os.environ.get('DBUSER'),
-        'PASSWORD': os.environ.get('DBPASS'),
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'require'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
